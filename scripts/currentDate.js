53cf14c5d3b85c0c58c1c921fa72b5471985fb73
SUGalib <sibgatullahshahidigalib31798@gmail.com>
2025-08-14 15:07:18 +0600

    html part completed

---
+++ scripts/currentDate.js
@@ -0,0 +1,4 @@
+ const dateElement = document.getElementById("dateDisplay");
+        const today = new Date();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        dateElement.textContent = today.toLocaleDateString('en-us', options);
